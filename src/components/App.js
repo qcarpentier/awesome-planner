6c73fdf39f48d7f286eb978d31ec54a4fe5f593f
Quentin <quentin.carpentier@outlook.be>
2018-08-26 23:50:28 +0200

    State datas inside App component

---
+++ src/components/App.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      today: [],
+      tomorrow: [],
+      dayAfterTomorrow: [],
+      username: 'Jon Snow'
+    }
+  }
+  
+  render() {
+    return (
+      <div className="App">
+        
+      </div>
+    );
+  }
+}
+
+export default App;
